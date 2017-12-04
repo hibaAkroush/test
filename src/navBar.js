4eeb763c93d040d0fd933177f2d745db904a9dae
hibaAkroush <hiba.akr@gmail.com>
2017-12-04 21:21:14 +0200

    nav

--- src/navBar.js
+++ src/navBar.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom';
 import './navBar.css';
 
 const element = (
-	<div>
+<div class="navbar navbar-inverse navbar-fixed-top opaque-navbar">
+  <div class="container">
+    <div class="navbar-header">
+	    <a class="navbar-brand" href="#">Actions</a>
+	    <a class="navbar-brand" href="#">Procurnment Overview Page</a>
+	    <a class="navbar-brand" href="#">Inbox</a>
+	    <a class="navbar-brand" href="#">News</a>
+	    <a class="navbar-brand" href="#">Requests</a>
+	    <a class="navbar-brand" href="#">Profile</a>
+	    <a class="navbar-brand" href="#">Letters</a>
+    </div>
+  </div>
+</div>
 
-	</div>
 	);
 ReactDOM.render(
   element,
