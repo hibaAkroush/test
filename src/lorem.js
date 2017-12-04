dd36e5c9ea822d6dd4460664b64482029ab79a9d
hibaAkroush <hiba.akr@gmail.com>
2017-12-04 23:25:21 +0200

    lorem

--- src/lorem.js
+++ src/lorem.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './lorem.css';
+import logo from './img/logo.png' 
+import logo1 from './img/logo1.png' 
+import logo2 from './img/logo2.png' 
 
 const element = (
 	<div class="contain">
@@ -13,40 +16,43 @@ const element = (
 			<h7>Neque porro</h7>
 		</div>
 		<div class="conta box1">
-	<h5>Lorem ipsum</h5>
-	<h7>Neque porro</h7>
+			<h5>Lorem ipsum</h5>
+			<h7>Neque porro</h7>
+			<img id="logo" src={logo}/>
 		</div>
 		<div class="conta">
-	<h5>Lorem ipsum</h5>
-	<h7>Neque porro</h7>
+			<h5>Lorem ipsum</h5>
+			<h7>Neque porro</h7>
 		</div>
 		<div class="conta">
-	<h5>Lorem ipsum</h5>
-	<h7>Neque porro</h7>
+			<h5>Lorem ipsum</h5>
+			<h7>Neque porro</h7>
 		</div>
 		<div class="conta box2">
-	<h5>Lorem ipsum</h5>
-	<h7>Neque porro</h7>
+			<h5>Lorem ipsum</h5>
+			<h7>Neque porro</h7>
+			<img id="logo1" src={logo1}/>
 		</div>
 		<div class="conta">
-	<h5>Lorem ipsum</h5>
-	<h7>Neque porro</h7>
+			<h5>Lorem ipsum</h5>
+			<h7>Neque porro</h7>
 		</div>
 		<div class="conta">
-	<h5>Lorem ipsum</h5>
-	<h7>Neque porro</h7>
+			<h5>Lorem ipsum</h5>
+			<h7>Neque porro</h7>
 		</div>
 		<div class="conta box3">
-	<h5>Lorem ipsum</h5>
-	<h7>Neque porro </h7>
+			<h5>Lorem ipsum</h5>
+			<h7>Neque porro </h7>
+			<img id="logo2" src={logo2}/>
 		</div>
 		<div class="conta">
-	<h5>Lorem ipsum</h5>
-	<h7>Neque porro</h7>
+			<h5>Lorem ipsum</h5>
+			<h7>Neque porro</h7>
 		</div>
 		<div class="conta">
-	<h5>Lorem ipsum</h5>
-	<h7>Neque porro</h7>
+			<h5>Lorem ipsum</h5>
+			<h7>Neque porro</h7>
 		</div>
 	</div>
 
